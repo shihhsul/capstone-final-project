70d187e88daa6d2aeb496c8e9e2157b216241d03
bcmgbokwere0 <bcmgbokwere@gmail.com>
2023-09-06 02:31:15 -0500

    Setup Functioning React + Vite + Tailwind

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 function App() {
-
   return (
     <div className='bg-indigo-600 min-h-screen'>
-    <h1 className="text-white"> Capstone Project </h1>
-    <p> Test! </p>
+      <div className='flex flex-col items-center pt-10'>
+        <h1 className='text-3xl font-medium text-white text-center'>
+          Added Tailwind Functionality To Our Project
+        </h1>
+        <span className='whitespace-pre'> </span>
+        <p className="text-white italic">Temporary Text</p>
+      </div>
     </div>
-
   )
 }
-
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html", "./src/**/*.{jsx,js,cjs,mjs,ts}"
-  ],
+  content: ["./index.html", "./Frontend/src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
