6849b9a778f6fbfc27328168a644490d815326a1
unknown <marsandnoa@gmail.com>
2023-09-18 12:52:49 -0400

    added initial stuff for new fish, but need to fix selected

--- Frontend/src/Pages/Edit.jsx
+++ Frontend/src/Pages/Edit.jsx
@@ -10,7 +10,14 @@ const AquariumPage = ({ aquarium }) => {
   const navigate = useNavigate();
   const [selectedFishSchool, setSelectedFishSchool] = useState(null);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [newFishSchoolAmount, setnewFishSchoolAmount] = useState('');
+  const [newFishSchoolAmount, setNewFishSchoolAmount] = useState('');
+  const [fishTypes] = useState([
+    { id: 1, name: 'Fish Type 1', scientificName: 'Scientific Name 1', speciesGroup: 'Species Group 1', lifespan: 'Lifespan 1', averageSize: 'Average Size 1', careLevel: 'Care Level 1', foodOptions: 'Food Options 1', foodType: 'Food Type 1', idealNumber: 'Ideal Number 1', isAggressiveToOtherSpecies: 'Yes', isAggressiveToSameSpecies: 'No', swimmingLevel: 'Swimming Level 1', temperatureRange: 'Temperature Range 1', phRange: 'pH Range 1', waterHardness: 'Water Hardness 1', minTankSize: 'Minimum Tank Size 1', substrate: 'Substrate 1', lightLevel: 'Light Level 1', livePlants: 'Yes', currentLevel: 'Current Level 1', decorationsPresent: 'Yes' },
+    { id: 2, name: 'Fish Type 2', scientificName: 'Scientific Name 2', speciesGroup: 'Species Group 2', lifespan: 'Lifespan 2', averageSize: 'Average Size 2', careLevel: 'Care Level 2', foodOptions: 'Food Options 2', foodType: 'Food Type 2', idealNumber: 'Ideal Number 2', isAggressiveToOtherSpecies: 'No', isAggressiveToSameSpecies: 'Yes', swimmingLevel: 'Swimming Level 2', temperatureRange: 'Temperature Range 2', phRange: 'pH Range 2', waterHardness: 'Water Hardness 2', minTankSize: 'Minimum Tank Size 2', substrate: 'Substrate 2', lightLevel: 'Light Level 2', livePlants: 'No', currentLevel: 'Current Level 2', decorationsPresent: 'No' },
+    { id: 3, name: 'Fish Type 3', scientificName: 'Scientific Name 3', speciesGroup: 'Species Group 3', lifespan: 'Lifespan 3', averageSize: 'Average Size 3', careLevel: 'Care Level 3', foodOptions: 'Food Options 3', foodType: 'Food Type 3', idealNumber: 'Ideal Number 3', isAggressiveToOtherSpecies: 'No', isAggressiveToSameSpecies: 'No', swimmingLevel: 'Swimming Level 3', temperatureRange: 'Temperature Range 3', phRange: 'pH Range 3', waterHardness: 'Water Hardness 3', minTankSize: 'Minimum Tank Size 3', substrate: 'Substrate 3', lightLevel: 'Light Level 3', livePlants: 'Yes', currentLevel: 'Current Level 3', decorationsPresent: 'Yes' }
+  ]);
+  const [selectedFishType, setSelectedFishType] = useState(null);
+  const [isFishTypeListVisible, setIsFishTypeListVisible] = useState(false);
 
   const handleFishSchoolSelect = (fishSchool) => {
     setSelectedFishSchool(fishSchool);
@@ -18,7 +25,7 @@ const AquariumPage = ({ aquarium }) => {
 
   const handleDelete = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:8080/fishschools/delete/"+selectedFishSchool.id, {
+      const response = await fetch(`http://127.0.0.1:8080/fishschools/delete/${selectedFishSchool.id}`, {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
@@ -27,116 +34,136 @@ const AquariumPage = ({ aquarium }) => {
 
       if (response.ok) {
         const updatedAquariums = aquarium.fishSchools.filter(fishSchool => fishSchool !== selectedFishSchool);
-        aquarium.fishSchools=updatedAquariums;
+        aquarium.fishSchools = updatedAquariums;
         navigate("/Edit", {});
       } else {
-        console.error("Login failed");
+        console.error("Delete failed");
       }
     } catch (error) {
-      console.error("There was a problem with the login request", error);
-    }
-  };
-
-  const handleEdit = () => {
-    setIsDialogOpen(true);
-  };
-
-  const handleDialogClose = () => {
-    setIsDialogOpen(false);
-  };
-
-  const handleDialogSave = async () => {
-    console.log("New aquarium name:", newFishSchoolAmount);
-
-    try {
-      const response = await fetch("http://127.0.0.1:8080/fishschools/modify/"+selectedFishSchool.id, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (response.ok) {
-        selectedFishSchool.amountFish = newFishSchoolAmount;
-        navigate("/Edit", {});
-      } else {
-        console.error("Login failed");
+        console.error("There was a problem with the delete request", error);
       }
-    } catch (error) {
-      console.error("There was a problem with the login request", error);
-    }
+    };
 
-    setIsDialogOpen(false);
-    setnewFishSchoolAmount('');
-  };
+    const handleEdit = () => {
+      setIsDialogOpen(true);
+    };
 
-  return (
-    <div>
-      <h2>Aquarium Name: {aquarium.name}</h2>
-      <ul>
-        {aquarium.fishSchools.map((fishSchool, i) => (
-          <li key={i} onClick={() => handleFishSchoolSelect(fishSchool)}>
-            Fish School {i + 1}: {fishSchool.name} <br />
-            Fish Name: {fishSchool.fishType.commonName} <br />
-            Fish Amount: {fishSchool.amountFish}
-          </li>
-        ))}
-      </ul>
-      <h2>Actions</h2>
-        <button>New</button>
-        <button onClick={handleEdit}>Edit</button>
-        <button onClick={handleDelete}>Delete</button>
+    const handleDialogClose = () => {
+      setIsDialogOpen(false);
+    };
 
-        {isDialogOpen && (
-        <div className="dialog">
-          <div className="dialog-content">
-            <h2>Create a New Aquarium</h2>
-            <input
-              type="text"
-              placeholder="Enter aquarium name"
-              value={newFishSchoolAmount}
-              onChange={(e) => setnewFishSchoolAmount(e.target.value)}
-            />
-            <button onClick={handleDialogSave}>Save</button>
-            <button onClick={handleDialogClose}>Cancel</button>
-          </div>
-        </div>
-      )}
+    const handleDialogSave = async () => {
+      console.log("New aquarium name:", newFishSchoolAmount);
 
-      {selectedFishSchool && (
+      try {
+        const response = await fetch(`http://127.0.0.1:8080/fishschools/modify/${selectedFishSchool.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ amountFish: newFishSchoolAmount })
+        });
+
+        if (response.ok) {
+          selectedFishSchool.amountFish = newFishSchoolAmount;
+          navigate("/Edit", {});
+        } else {
+          console.error("Save failed");
+        }
+      } catch (error) {
+          console.error("There was a problem with the save request", error);
+        }
+
+        setIsDialogOpen(false);
+        setNewFishSchoolAmount('');
+      };
+
+      const handleNew = () => {
+        setIsFishTypeListVisible(true);
+        setSelectedFishType(null);
+      };
+
+      return (
         <div>
-          <h3>Viewing Fish School: {selectedFishSchool.name}</h3>
-          <h3>Species Information:</h3>
-          <p>Fish Name: {selectedFishSchool.fishType.commonName}</p>
-          <p>Scientific Name: {selectedFishSchool.fishType.commonName}</p>
-          <p>Species Group: {selectedFishSchool.fishType.commonName}</p>
-          <p>Lifespan: {selectedFishSchool.fishType.commonName}</p>
-          <p>Average Size: {selectedFishSchool.fishType.commonName}</p>
+          <h2>Aquarium Name: {aquarium.name}</h2>
+          <ul>
+            {aquarium.fishSchools.map((fishSchool, i) => (
+              <li key={i} onClick={() => handleFishSchoolSelect(fishSchool)}>
+                Fish School {i + 1}: {fishSchool.name} <br />
+                Fish Name: {fishSchool.fishType.commonName} <br />
+                Fish Amount: {fishSchool.amountFish}
+              </li>
+            ))}
+          </ul>
+          <h2>Actions</h2>
+          <button onClick={handleNew}>New</button>
+          <button onClick={() => handleEdit(selectedFishSchool)}>Edit</button>
+          <button onClick={() => handleDelete(selectedFishSchool)}>Delete</button>
 
-          <h3>Fish Care Details</h3>
-          <p>Care Level: {selectedFishSchool.fishType.commonName}</p>
-          <p>Food Options: {selectedFishSchool.fishType.commonName}</p>
-          <p>FoodType: {selectedFishSchool.fishType.commonName}</p>
-          <p>Ideal Number: {selectedFishSchool.fishType.commonName}</p>
-          <p>Aggressive to other Species?: {selectedFishSchool.fishType.commonName}</p>
-          <p>Aggressive to same Species?: {selectedFishSchool.fishType.commonName}</p>
-          <p>Swimming Level: {selectedFishSchool.fishType.commonName}</p>
+          {isFishTypeListVisible && (
+            <div>
+              <h3>Fish Types</h3>
+              <ul>
+                {fishTypes.map((fishType) => (
+                  <li key={fishType.id}>
+                    {fishType.name}
+                    <button onClick={() => setSelectedFishType(fishType)}>Select</button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
 
-          <h3>Tank Requirements:</h3>
-          <p>Temperature Range: {selectedFishSchool.fishType.commonName}</p>
-          <p>pH Range: {selectedFishSchool.fishType.commonName}</p>
-          <p>Water Hardness: {selectedFishSchool.fishType.commonName}</p>
-          <p>Minimum Tank Size: {selectedFishSchool.fishType.commonName}</p>
-          <p>Substrate: {selectedFishSchool.fishType.commonName}</p>
-          <p>Light Level: {selectedFishSchool.fishType.commonName}</p>
-          <p>Live Plants: {selectedFishSchool.fishType.commonName}</p>
-          <p>Current Level: {selectedFishSchool.fishType.commonName}</p>
-          <p>Decorations Present: {selectedFishSchool.fishType.commonName}</p>
+          {selectedFishSchool && (
+            <div>
+              <h3>Viewing Fish School: {selectedFishSchool.name}</h3>
+              <h3>Species Information:</h3>
+              <p>Fish Name: {selectedFishSchool.fishType.commonName}</p>
+              <p>Scientific Name: {selectedFishSchool.fishType.scientificName}</p>
+              <p>Species Group: {selectedFishSchool.fishType.speciesGroup}</p>
+              <p>Lifespan: {selectedFishSchool.fishType.lifespan}</p>
+              <p>Average Size: {selectedFishSchool.fishType.averageSize}</p>
+
+              <h3>Fish Care Details</h3>
+              <p>Care Level: {selectedFishSchool.fishType.careLevel}</p>
+              <p>Food Options: {selectedFishSchool.fishType.foodOptions}</p>
+              <p>FoodType: {selectedFishSchool.fishType.foodType}</p>
+              <p>Ideal Number: {selectedFishSchool.fishType.idealNumber}</p>
+              <p>Aggressive to other Species?: {selectedFishSchool.fishType.isAggressiveToOtherSpecies}</p>
+              <p>Aggressive to same Species?: {selectedFishSchool.fishType.isAggressiveToSameSpecies}</p>
+              <p>Swimming Level: {selectedFishSchool.fishType.swimmingLevel}</p>
+
+              <h3>Tank Requirements:</h3>
+              <p>Temperature Range: {selectedFishSchool.fishType.temperatureRange}</p>
+              <p>pH Range: {selectedFishSchool.fishType.phRange}</p>
+              <p>Water Hardness: {selectedFishSchool.fishType.waterHardness}</p>
+              <p>Minimum Tank Size: {selectedFishSchool.fishType.minTankSize}</p>
+              <p>Substrate: {selectedFishSchool.fishType.substrate}</p>
+              <p>Light Level: {selectedFishSchool.fishType.lightLevel}</p>
+              <p>Live Plants: {selectedFishSchool.fishType.livePlants}</p>
+              <p>Current Level: {selectedFishSchool.fishType.currentLevel}</p>
+              <p>Decorations Present: {selectedFishSchool.fishType.decorationsPresent}</p>
+            </div>
+          )}
+
+          {isDialogOpen && (
+            <div className="dialog">
+              <div className="dialog-content">
+                <h2>Edit Fish School</h2>
+                <input
+                  type="text"
+                  placeholder="Enter new amount of fish"
+                  value={newFishSchoolAmount}
+                  onChange={(e) => setNewFishSchoolAmount(e.target.value)}
+                />
+                <button onClick={handleDialogSave}>Save</button>
+                <button onClick={handleDialogClose}>Cancel</button>
+              </div>
+            </div>
+          )}
         </div>
-      )}
-    </div>
-  );
-};
+      );
+    };
 
 const Edit = () => {
   const { userData } = useContext(UserContext);
