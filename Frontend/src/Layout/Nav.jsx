e809fc05cffa769955d0dab95ca4c5fef9746f67
bcmgbokwere0 <bcmgbokwere@gmail.com>
2023-09-26 02:37:24 -0500

    About Page design and routing changes

--- Frontend/src/Layout/Nav.jsx
+++ Frontend/src/Layout/Nav.jsx
@@ -1,27 +1,19 @@
 import { Link } from "react-router-dom";
-import logo from "../App Components/Fish.png";
+import logo from "../App Components/clown-fish.png";
 
 const Nav = () => {
   return (
-    <header className='w-full h-20 flex items-center justify-between bg-blue-600 sticky p-10'>
-      <div className='text-2xl text-white font-bold flex items-center'>
+    <header className='w-full h-12 flex items-center justify-between bg-blue-900 sticky p-4'>
+      <div className='text-xl text-white font-bold flex items-center'>
         Finding Nemo
-        <img className='w-10 h-15' src={logo} alt='Fish' />
+        <img className='w-8 h-8 m-2' src={logo} alt='Fish' />
       </div>
       <nav>
         <ul className='list-none m-0 p-0 flex items-baseline'>
           <li className='ml-10'></li>
           <li className='ml-5'>
             <Link
-              className='text-2xl text-white font-bold no-underline hover:opacity-80'
-              to='/Main'
-            >
-              Main
-            </Link>
-          </li>
-          <li className='ml-5'>
-            <Link
-              className='text-2xl text-white font-bold no-underline hover:opacity-80'
+              className='text-xl text-white font-bold no-underline hover:opacity-80'
               to='/About'
             >
               About
@@ -29,7 +21,15 @@ const Nav = () => {
           </li>
           <li className='ml-5'>
             <Link
-              className='text-2xl text-white font-bold no-underline hover:opacity-80'
+              className='text-xl text-white font-bold no-underline hover:opacity-80'
+              to='/Main'
+            >
+              Main
+            </Link>
+          </li>
+          <li className='ml-5'>
+            <Link
+              className='text-xl text-yellow-400 font-bold no-underline hover:opacity-80'
               to='/'
             >
               Login
